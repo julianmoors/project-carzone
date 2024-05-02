4d1528a2b82b536734610557bcb1857e67298bf9
Julian Moors <julian.moors@outlook.com>
2024-05-02 19:49:45 +0100

    general update

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
+
 from cars.models import Car
 
 def register_view(request):
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+
 from cars.models import Car
 from . models import Member
 
